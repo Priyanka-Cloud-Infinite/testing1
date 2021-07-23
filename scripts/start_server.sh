36845cdc7b7b56512977f6cd41083558010e7605
Priyanka Patel <83409681+Priyanka-Cloud-Infinite@users.noreply.github.com>
2021-07-23 11:06:13 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
-c=$(ls -l /var/www/html/ | grep "^d" | wc -l)
-arr1=(/var/www/html/*)
+c=$(ls -l /var/www/testing1/ | grep "^d" | wc -l)
+arr1=(/var/www/testing1/*)
 echo ${arr1[@]}
 total=${#arr1[@]}
 for(( i=0; i<$total; i++ ))
